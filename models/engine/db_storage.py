2242d014d77464aae764657b1e65251de619c1b6
LusterPearl <salamioluwatosin90@gmail.com>
2023-12-19 16:15:12 -0800

    json error

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,8 +2,11 @@
 """ Storage for DBStorage to create new engine """
 
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import scoped_session
+from models.base_model import Base
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -13,43 +16,42 @@ from models.place import Place
 from models.review import Review
 from os import getenv
 
-Base = declarative_base()
-
 
 class DBStorage:
     """Database storage engine using SQLALchemy"""
     __engine = None
     __session = None
 
+
     def __init__(self):
         """Initialization method"""
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(getenv('HBNB_MYSQL_USER'),
-                                              getenv('HBNB_MYSQL_PWD'),
-                                              getenv('HBNB_MYSQL_HOST'),
-                                              getenv('HBNB_MYSQL_DB')),
-                                      echo=True,
-                                      pool_pre_ping=True)
+        user = getenv("HBNB_MYSQL_USER")
+        pwd = getenv("HBNB_MYSQL_PWD")
+        host = getenv("HBNB_MYSQL_HOST", default="localhost")
+        db = getenv("HBNB_MYSQL_DB")
+        env = getenv("HBNB_ENV")
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}:3306/{}".
+                                     format(user, pwd, host, db),
+                                     pool_pre_ping=True)
 
-        if getenv('HBNB_ENV') == 'test':
+        if env == "test":
             Base.metadata.drop_all(self.__engine)
+            Base.metadata.create_all(self.__engine)
 
     def all(self, cls=None):
         """Queries objects depending on the class name"""
-        query_dict = {}
-        classes = [City, State]
         if cls is None:
-            for c in classes:
-                objs = self.__session.query(c).all()
-                for obj in objs:
-                    key = '{}{}'.format(obj.__class__.__name__, obj.id)
-                    query_dict[key] = obj
+            objs = self.__session.query(State).all()
+            objs.extend(self.__session.query(City).all())
+            objs.extend(self.__session.query(User).all())
+            objs.extend(self.__session.query(Place).all())
+            objs.extend(self.__session.query(Review).all())
+            objs.extend(self.__session.query(Amenity).all())
         else:
-            objs = self.__session.query(cls).all()
-            for obj in objs:
-                key = '{}.{}'.format(obj.__class__.__name__, obj.id)
-                query_dict[key] = obj
-        return query_dict
+            if type(cls) == str:
+                cls = eval(cls)
+            objs = self.__session.query(cls)
+        return {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
 
     def new(self, obj):
         """Adds the object to the current database session"""
@@ -61,13 +63,16 @@ class DBStorage:
 
     def delete(self, obj=None):
         """Deletes the object from the current database session"""
-        if obj:
+        if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
         """creates all tbles in the database and creates a new session"""
-        Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(
                            bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """ close all the working session """
+        self.__session.remove()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from os import environ
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import models
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete",)
