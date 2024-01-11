6f524147acfe3d3017d3e57573d0e670f17a2ee4
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-01-11 03:51:38 -0800

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,58 +1,62 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-Fabric script (based on the file 1-pack_web_static.py) that distributes
-an archive to your web servers
+Fabric script (based on 1-pack_web_static.py) that distributes an archive
+to your web servers, using the function do_deploy.
 """
-from fabric.api import *
-import os
+
+from fabric.api import local, put, run, env
 from datetime import datetime
+import os
 
-env.hosts = ['100.26.168.142', '100.25.35.218']
+env.hosts = ['52.206.18.131', '54.157.130.186']
 env.user = 'ubuntu'
-env.key_filename = 'private_key.txt'
+env.key_filename = 'private-key.txt'
 
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static folder.
+    """
+    try:
+        if not os.path.exists("versions"):
+            local("mkdir versions")
+        now = datetime.now()
+        timestamp = now.strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(timestamp)
+        local("tar -cvzf {} web_static".format(archive_path))
+        return archive_path
+    except Exception as e:
+        return None
 
 def do_deploy(archive_path):
     """
-    Distributes an archive to your web servers
+    Deploys an archive to the web servers.
     """
     if not os.path.exists(archive_path):
         return False
 
     try:
-        archive_filename = os.path.basename(archive_path)
-        archive_no_ext = os.path.splitext(archive_filename)[0]
-        """ Upload the archive to tmp directory on the web server """
         put(archive_path, '/tmp/')
 
-        """ Create the folder for the new version """
-        run('mkdir -p /data/web_static/releases/{}/'.format(archive_no_ext))
+        archive_filename = os.path.basename(archive_path)
+        release_folder = '/data/web_static/releases/{}'.format(
+            archive_filename.split('.')[0]
+        )
+        run('mkdir -p {}'.format(release_folder))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_folder))
 
-        """ Uncompress the archive to the folder """
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
-            archive_filename, archive_no_ext))
-
-        """ Delete the archive from the web server """
         run('rm /tmp/{}'.format(archive_filename))
 
-        """ Move the contents to the proper directory """
-        run('mv / data/web_static/releases/{}/web_static/*
-                / data/web_static/releases/{} /'
-            .format(archive_no_ext, archive_no_ext))
+        """ Move contents to proper location and clean up """
+        run('mv {}/web_static/* {}'.format(release_folder, release_folder))
+        run('rm -rf {}/web_static'.format(release_folder))
 
-        """ Delete the uncessary folder """
-        run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(archive_no_ext))
-
-        """ Delete the symbolic link /data/web_static/current"""
+        """ Delete the current symbolic link """
         run('rm -rf /data/web_static/current')
 
-        """ Create a ne symbolic link """
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-            .format(archive_no_ext))
+        run('ln -s {} /data/web_static/current'.format(release_folder))
 
         print("New version deployed!")
         return True
-
-    except Excetion as e:
+    except Exception as e:
         return False
+
