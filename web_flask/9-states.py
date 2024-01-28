b847ff6f0624e8dfe44eb7dee949a5b0bc202298
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-01-28 19:43:37 +0000

    9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
+"""Script that starts a Flask web application."""
+
 from flask import render_template
 from flask import Flask
 from models import storage
 from models.state import State
 
-
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def close_session(self):
-    """remove the current SQLAlchemy Session"""
+def close_session(exception):
+    """Remove the current SQLAlchemy Session."""
     storage.close()
 
 
 @app.route('/states', strict_slashes=False)
 @app.route('/states/<id>', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
 def display_states(id=None):
-    """display states and cities"""
+    """Display states and cities."""
     states = storage.all(State)
+    state = None
+    
     if id:
-        id = 'State.' + id
-    return render_template('9-states.html', states=states, id=id)
+        state_id = 'State.' + id
+        state = states.get(state_id)
+
+    return render_template('9-states.html', states=states, state=state)
 
 
 if __name__ == '__main__':
